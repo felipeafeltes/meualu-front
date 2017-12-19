616179ea45182eb286fb5dfbeee3d4a5d96649f1
Caciano kroth <caciano.kroth@ubistart.com>
2017-12-19 17:50:25 -0200

    ajuste body

--- js/controllers/profileController.js
+++ js/controllers/profileController.js
@@ -6,12 +6,10 @@
         if ($rootScope.current_user === undefined || $rootScope.current_user === null) {
             if (localStorage.getItem('token')) {
                 $scope.hasData = false;
-                $('body').css('overflow', 'hidden');
-                //DADOS DO USUARIO
+                //DADOS DO USUARIOO
                 MySelf.get(
                     {},
                     function (data) {
-                        $('body').css('overflow', 'auto');
                         $scope.hasData = true;
                         $rootScope.current_user = data.renter;
                         ($rootScope.current_user.birthday !== null) ? $rootScope.current_user.birthday = new Date(data.renter.birthday) : '';
