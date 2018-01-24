e4f9cd70573f314396690995923bfd286dd5acc7
Caciano kroth <caciano.kroth@ubistart.com>
2018-01-24 18:34:11 -0200

    up

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,36 +2,102 @@ module.exports = function (grunt) {
     //grunt wrapper function 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        /*         ngAnnotate: {
-                    options: {
-                        singleQuotes: true
-                    },
-                    app: {
-                        files: {
-                            './js/controllers/profile/editProfileController.js': ['./public/js/appFactory.js'],
-                        }
-                    }
-                }, */
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.dist/*'
+                    ]
+                }]
+            }
+        },
+        less: {
+            development: {
+                files: {
+                    "./dist/css/app.css": "./assets/*.css"
+                }
+            },
+            production: {
+                options: {
+                    yuicompress: true
+                },
+                files: {
+                    "./dist/css/app.css": "./assets/*.css"
+                }
+            }
+        },
         concat: {
-            js: { //target
-                src: ['./public/min-safe/app.js', './public/min-safe/js/*.js'],
-                dest: './dist/min/app.js'
+            js: {
+                files: {
+                    './dist/min/min.js': ['./js/controllers/**/*.js'],
+                    './dist/min/directives.js': ['./js/directives/**/*.js'],
+                    './dist/min/services.js': ['./js/services/**/*.js'],
+                }
             }
         },
         uglify: {
-            js: { //target
-                files: {
-                    'build/min/app.min.js': ['.js/app.js']
-                }
+            js: {
+                src: ['./dist/min/app.js'],
+                dest: './dist/main.js'
             }
-        }
-    });
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    useShortDoctype: true,
+                    collapseWhitespace: true,
+                    removeEmptyAttributes: true,
+                    removeCDATASectionsFromCDATA: true
+                },
+                files: [{
+                    expand: true,
+                    src: ['./views/*.html'],
+                    dest: './dist/views'
+                }]
+            }
+        },
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './*',
+                    dest: './dist',
+                    src: [
+                        '*.html',
+                        '*.{ico,txt}',
+                        '.htaccess',
+                        //'components/**/*',
+                        'img/{,*/}*.{gif,webp}',
+                        'css/fonts/*'
+                    ]
+                }]
+            }
+        },
 
+    });
     //load grunt tasks
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    /*     grunt.loadNpmTasks('grunt-ng-annotate'); */
 
     //register grunt default task
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('build', ['concat']);
+    grunt.registerTask('min', ['uglify']);
+
+
+    // Default task.
+    grunt.registerTask('build', [
+        'clean:dist',
+        'less:production',
+        'concat',
+        'cssmin',
+        'copy',
+        'uglify',
+        'usemin',
+        'htmlmin'
+    ]);
+
+    grunt.registerTask('default', ['build']);
 }
